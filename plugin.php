adc7c046f005afa68395ad3d8f892cb7accb611a
smeyer <smeyer.ilias@gmx.de>
2020-11-05 12:31:57 +0100

    Increased plugin version

--- plugin.php
+++ plugin.php
@@ -4,15 +4,12 @@
 $id = "xmg";
  
 // code version; must be changed for all code changes
-define('xmg_version', '5.4.0.1');
+define('xmg_version', '6.5.1');
 $version = xmg_version;
  
-// ilias min and max version; must always reflect the versions that should
-// run with the plugin
-$ilias_min_version = "5.4.0";
-$ilias_max_version = "5.4.999";
+$ilias_min_version = "6.5";
+$ilias_max_version = "6.99";
  
-// optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Stefan Meyer';
 $responsible_mail = 'meyer@leifos.de';
 ?>
